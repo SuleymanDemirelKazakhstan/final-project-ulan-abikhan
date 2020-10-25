b4925bbaac27e4d0aa8233d804a30c4a8fd22fc4
Ulan  Abikhan <u.abikhan02@mail.ru>
2020-10-25 18:36:02 +0600

    fourth commit

--- Project/script.js
+++ Project/script.js
@@ -5,3 +5,11 @@ for (i = 0;i < 4;i++) {
 function func() {
 	console.log("hello");
 }
+var express = require('express');
+var app = express();
+var server = app.listen(3000, listening);
+function listening() {}
+for (i = 0;i < 1000;i++) {
+	console.log("Hello, world!");
+}
+app.use(express.static('website'));
